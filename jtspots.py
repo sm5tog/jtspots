e422b56f690b1fa14b7c17d6de7c5f9f7653ae65
Jonny <Jonny.majava@mac.com>
2026-06-26 23:08:49 +0200

    Restore password field, send all four required Clublog params

--- jtspots.py
+++ jtspots.py
@@ -154,10 +154,11 @@ class ClublogClient:
     def _fetch_matrix_bg(self, on_done):
         try:
             params = urllib.parse.urlencode({
-                'call':  self.callsign,
-                'api':   self.api_key,
-                'email': self.email,
-                'mode':  0,
+                'call':     self.callsign,
+                'api':      self.api_key,
+                'email':    self.email,
+                'password': self.password,
+                'mode':     0,
             })
             url = f'{self.MATRIX_URL}?{params}'
             debug_url = re.sub(r'(password=)[^&]+', r'\1***', url)
@@ -386,9 +387,13 @@ class JTSpots(ctk.CTk):
         self._mk_label(cf, 'E-post:', 1, 0)
         self._e_cl_email = self._mk_entry(cf, '', 1, 1, 200)
 
-        self._mk_label(cf, 'API-nyckel:', 2, 0)
+        self._mk_label(cf, 'Lösenord:', 2, 0)
+        self._e_cl_pass = ctk.CTkEntry(cf, width=200, show='*')
+        self._e_cl_pass.grid(row=2, column=1, sticky='w', padx=4, pady=3)
+
+        self._mk_label(cf, 'API-nyckel:', 3, 0)
         self._e_cl_api = ctk.CTkEntry(cf, width=200, show='*')
-        self._e_cl_api.grid(row=2, column=1, sticky='w', padx=4, pady=3)
+        self._e_cl_api.grid(row=3, column=1, sticky='w', padx=4, pady=3)
 
         btn_row = ctk.CTkFrame(cf, fg_color='transparent')
         btn_row.grid(row=4, column=0, columnspan=4, sticky='w', padx=6, pady=(2, 6))
@@ -491,7 +496,7 @@ class JTSpots(ctk.CTk):
     def _fetch_clublog(self):
         self._clublog.email    = self._e_cl_email.get().strip()
         self._clublog.api_key  = self._e_cl_api.get().strip()
-        self._clublog.password  = ''
+        self._clublog.password  = self._e_cl_pass.get()
         self._clublog.callsign  = self._e_call.get().strip()
         self._lbl_cl_status.configure(text='Hämtar...', text_color='gray')
         self._clublog.fetch_matrix(on_done=self._on_clublog_done)
@@ -593,6 +598,7 @@ class JTSpots(ctk.CTk):
             'tport':    self._e_tport.get(),
             'callsign': self._e_call.get(),
             'cl_email': self._e_cl_email.get(),
+            'cl_pass':  self._e_cl_pass.get(),
             'cl_api':   self._e_cl_api.get(),
             'flt_cq':   self._flt_cq.get(),
             'flt_snr':  self._flt_snr.get(),
@@ -618,6 +624,7 @@ class JTSpots(ctk.CTk):
         set_entry(self._e_tport,    'tport')
         set_entry(self._e_call,     'callsign')
         set_entry(self._e_cl_email, 'cl_email')
+        set_entry(self._e_cl_pass,  'cl_pass')
         set_entry(self._e_cl_api,   'cl_api')
         set_entry(self._e_snr,      'snr')
         if 'flt_cq'      in data: self._flt_cq.set(data['flt_cq'])
